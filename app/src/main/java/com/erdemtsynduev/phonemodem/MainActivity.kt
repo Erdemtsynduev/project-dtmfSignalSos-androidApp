91513a5635b59999eb7bccf8ff3575bdb6fa11e7
Erdem Tsynduev <erdemtsynduev@gmail.com>
2021-03-21 13:52:23 +0300

    feat: Add buttons call sos

--- app/src/main/java/com/erdemtsynduev/phonemodem/MainActivity.kt
+++ app/src/main/java/com/erdemtsynduev/phonemodem/MainActivity.kt
@@ -4,13 +4,16 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startCaller()
+        btn_sos.setOnClickListener {
+            startCaller()
+        }
     }
 
     private fun startCaller() {
